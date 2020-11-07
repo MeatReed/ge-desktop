da6af7d0d0d50218de4fb6c9cd6b7347b2a361de
Stereo 18 <stereogaming18@gmail.com>
2020-11-07 19:04:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+<p align="center"><img src="icon.png" width="150px" draggable="false" height="150px"></p>
+
+<h1 align="center">Ghost eShop Desktop</h1>
+
+
+
+<h3 align="center">Stay alive !</h3>
+<a align="center" href="https://ghosteshop.com/" target="_blank"><p align="center">Go to website</p></a>
+
+
+## Download
+
+You can download from the [GitHub Releases](https://github.com/ghost-land/ge-desktop/releases). If there is no build for your platform, build with the dev kit
+
+## Use it
+
+To use it, select a game, scan the qr with FBI's remote installer
+
+## Developement
+
+**What do you need ?**
+
+* Node.js v12
+* Dependences with ```npm install```
+
+---
+
+**Clone and installation of dependences**
+
+```console
+> git clone https://github.com/ghost-land/ge-desktop.git
+> cd NumWorks-Emulator
+> npm install
+```
+
+---
+
+**Start the app**
+
+```console
+> npm start
+```
+
+---
+
+**Build the app**
+
+Build for your platform
+
+```console
+> npm run dist
+```
+
+| Platform    | Command              |
+| ----------- | -------------------- |
+| Windows x64 | `npm run dist:win`   |
+| macOS       | `npm run dist:mac`   |
+| Linux x64   | `npm run dist:linux` |
+
---
+++ index.js
@@ -0,0 +1,94 @@
+// Requirements
+const {app, BrowserWindow, ipcMain} = require('electron')
+const Menu                          = require('electron').Menu
+const ejse                          = require('ejs-electron')
+const fs                            = require('fs')
+const path                          = require('path')
+const semver                        = require('semver')
+const url                           = require('url')
+const axios = require('axios');
+let thisversion='1.0'
+// const low = require('lowdb')
+// const FileSync = require('lowdb/adapters/FileSync')
+ 
+// const adapter = new FileSync('db.json')
+// const db = low(adapter)
+
+// Disable hardware acceleration.
+// https://electronjs.org/docs/tutorial/offscreen-rendering
+app.disableHardwareAcceleration()
+
+// https://github.com/electron/electron/issues/18397
+app.allowRendererProcessReuse = true
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow() {
+
+    win = new BrowserWindow({
+        width: 1200,
+        height: 650,
+        icon: 'icon.png',
+        menu: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
+    axios.get('http://ged.stereo18.ml/status.json')
+  .then(function (response) {
+    if (response.data.test!='true'){
+        var target = url.format({
+            pathname: path.join(__dirname, 'app', 'noco.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    }
+    else if(response.data.version!=thisversion){
+        var target = url.format({
+            pathname: path.join(__dirname, 'app', 'outofdate.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    }
+    else{
+        var target = 'http://ged.stereo18.ml'
+    }
+    
+  win.loadURL(target)
+  })
+
+
+
+    /*win.once('ready-to-show', () => {
+        win.show()
+    })*/
+
+    win.removeMenu()
+
+    win.resizable = true
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
+})
